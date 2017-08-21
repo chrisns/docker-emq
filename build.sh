4a3ccfba28300fd87b0dc571404cd152408d91e6
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-08-21 17:39:44 +0100

    use the previous built docker image as a base

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-VERSIONS=$(git ls-remote --tags https://github.com/emqtt/emq-relx.git | awk  '{ print $2 }' | sed 's/refs\/tags\///g' | sed 's/\^{}//g' | uniq | rev)
+VERSIONS=$(git ls-remote --tags https://github.com/emqtt/emq-relx.git | awk  '{ print $2 }' | sed 's/refs\/tags\///g' | sed 's/\^{}//g' | uniq)
+
+echo $VERSIONS
 
 cat emq-docker-master/Dockerfile | sed 's/ENV EMQ_VERSION/ARG EMQ_VERSION/g' > emq-docker-master/Dockerfile.patched
-
-cp -f emq-docker-master/Dockerfile.patched emq-docker-master/Dockerfile.patched-hacked
-echo "COPY ../hacked-start.sh /opt/emqttd/" >> emq-docker-master/Dockerfile.patched-hacked
-echo "CMD /opt/emqttd/hacked-start.sh" >> emq-docker-master/Dockerfile.patched-hacked
+cp hacked-start.sh emq-docker-master
 
 function docker_tag_exists() {
     curl --silent -f -lSL https://index.docker.io/v1/repositories/$1/tags/$2 > /dev/null
@@ -17,7 +16,7 @@ for VERSION in ${VERSIONS} ; do \
     echo ${VERSION} already exists
   else
     echo ${VERSION} does not yet exist
-    docker build -t chrisns/emq:${VERSION}        --build-arg EMQ_VERSION=${VERSION} -f emq-docker-master/Dockerfile.patched        emq-docker-master
+    docker build -t chrisns/emq:${VERSION} --build-arg EMQ_VERSION=${VERSION} -f emq-docker-master/Dockerfile.patched emq-docker-master
     docker push chrisns/emq:${VERSION}
   fi
 
@@ -27,9 +26,9 @@ for VERSION in ${VERSIONS} ; do \
   else
     echo ${VERSION}-hacked does not yet exist
     echo "FROM chrisns/emq:${VERSION}" > emq-docker-master/Dockerfile.patched-hacked
-    echo "COPY ../hacked-start.sh /opt/emqttd/" >> emq-docker-master/Dockerfile.patched-hacked
+    echo "COPY hacked-start.sh /opt/emqttd/" >> emq-docker-master/Dockerfile.patched-hacked
     echo "CMD /opt/emqttd/hacked-start.sh" >> emq-docker-master/Dockerfile.patched-hacked
-    docker build -t chrisns/emq:${VERSION}-hacked --build-arg EMQ_VERSION=${VERSION} -f emq-docker-master/Dockerfile.patched-hacked emq-docker-master
+    docker build -t chrisns/emq:${VERSION}-hacked -f emq-docker-master/Dockerfile.patched-hacked emq-docker-master
     docker push chrisns/emq:${VERSION}-hacked
   fi
 
