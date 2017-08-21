c2665a4b49a14a6e58d985ecbd3924113356ffa6
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-08-21 17:03:12 +0100

    build a custom hacked image that'll bring up a DNS cluster

--- build.sh
+++ build.sh
@@ -4,20 +4,23 @@ VERSIONS=$(git ls-remote --tags https://github.com/emqtt/emq-relx.git | awk  '{ 
 
 cat emq-docker-master/Dockerfile | sed 's/ENV EMQ_VERSION/ARG EMQ_VERSION/g' > emq-docker-master/Dockerfile.patched
 
+cp -f emq-docker-master/Dockerfile.patched emq-docker-master/Dockerfile.patched-hacked
+echo "COPY ../hacked-start.sh /opt/emqttd/" >> emq-docker-master/Dockerfile.patched-hacked
+echo "CMD /opt/emqttd/hacked-start.sh" >> emq-docker-master/Dockerfile.patched-hacked
 
 function docker_tag_exists() {
     curl --silent -f -lSL https://index.docker.io/v1/repositories/$1/tags/$2 > /dev/null
 }
-
-
 
 for VERSION in ${VERSIONS} ; do \
   if docker_tag_exists chrisns/emq ${VERSION}; then
     echo ${VERSION} already exists
   else
     echo ${VERSION} does not yet exist
-    docker build -t chrisns/emq:${VERSION} --build-arg EMQ_VERSION=${VERSION} -f emq-docker-master/Dockerfile.patched emq-docker-master
+    docker build -t chrisns/emq:${VERSION}        --build-arg EMQ_VERSION=${VERSION} -f emq-docker-master/Dockerfile.patched        emq-docker-master
     docker push chrisns/emq:${VERSION}
+    docker build -t chrisns/emq:${VERSION}-hacked --build-arg EMQ_VERSION=${VERSION} -f emq-docker-master/Dockerfile.patched-hacked emq-docker-master
+    docker push chrisns/emq:${VERSION}-hacked
   fi
 done
 
---
+++ hacked-start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export EMQ_HOST=$(ip route get $(getent hosts ${EMQ_CLUSTER__DNS__NAME} | awk '{print $1}') | awk '{print $NF}')
+echo Using $EMQ_HOST
+sed -i 's/## cluster.dns/cluster.dns/g' /opt/emqttd/etc/emq.conf
+/opt/emqttd/start.sh
